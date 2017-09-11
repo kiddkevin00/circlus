ddd6c5402ef691aecfcb540a41a27cdb768a9942
Marcus Hsu <mh3346@columbia.edu>
2017-09-11 09:26:14 -0400

    Setup React Native project with default generated files and customize environment settings

---
+++ index.android.js
@@ -0,0 +1,18 @@
+import Landing from './src/lib/src/app/components/Landing';
+import { AppRegistry } from 'react-native';
+import React, { Component } from 'react';
+
+
+class Circlus extends Component {
+
+  render() {
+    return (
+      <Landing />
+    );
+  }
+
+}
+
+AppRegistry.registerComponent('LocalDetour', () => Circlus);
+
+export default Circlus;
---
+++ index.ios.js
@@ -0,0 +1,42 @@
+import Landing from './src/lib/src/app/components/Landing';
+import configureStore from './src/lib/src/app/store/';
+import {
+  NavigatorIOS,
+  AppRegistry,
+  StyleSheet,
+} from 'react-native';
+import { Provider } from 'react-redux';
+import React, { Component } from 'react';
+
+
+console.ignoredYellowBox = ['Using <Image> with children is deprecated and will be an error in the near future. Please reconsider the layout or use <ImageBackground> instead.'];
+
+const store = configureStore();
+const styles = StyleSheet.create({
+  container: {
+    flexGrow: 1,
+  },
+});
+
+class Circlus extends Component {
+
+  render() {
+    return (
+      <Provider store={ store }>
+        <NavigatorIOS
+          style={ styles.container }
+          navigationBarHidden={ true }
+          initialRoute={ {
+            title: 'Title',
+            component: Landing,
+          } }
+        />
+      </Provider>
+    );
+  }
+
+}
+
+AppRegistry.registerComponent('Circlus', () => Circlus);
+
+export { Circlus as default };
